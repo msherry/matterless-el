b9b93467b32cef0053aad2ef7ca60ae26aa765eb
Gaelan D'costa <gdcosta@gmail.com>
2018-10-15 21:37:33 -0400

    add ERT (unit test) generated test skeleton

---
+++ test/matterless-test.el
@@ -0,0 +1,3 @@
+;;; matterless-test.el --- Tests for matterless
+
+;;; matterless-test.el ends here
---
+++ test/test-helper.el
@@ -0,0 +1,3 @@
+;;; test-helper.el --- Helpers for matterless-test.el
+
+;;; test-helper.el ends here
